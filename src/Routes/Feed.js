f21d7baac1fd0bd26e06f7fd21e0a542b1da35a0
DesignC <znehraks@gmail.com>
2019-12-16 17:20:54 +0900

    Feed.js

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,13 +1,47 @@
 import React from "react";
 import styled from "styled-components";
 import { gql } from "apollo-boost";
-import { useQuery } from "react-apollo-hooks";
+import { useQuery } from "@apollo/react-hooks";
 import Loader from "../Components/Loader";
 
-// const FEED_QUERY = gql`
-//     {
-//         seeFeed{
-//             id
-//         }
-//     }
-// `;
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        userName
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          userName
+        }
+      }
+      createdAt
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 80vh;
+`;
+
+export default () => {
+  const { data, loading } = useQuery(FEED_QUERY);
+  return <Wrapper>{loading && <Loader />}</Wrapper>;
+};
